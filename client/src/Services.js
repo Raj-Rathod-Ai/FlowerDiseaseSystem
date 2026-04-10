d296ec8d1898db16efcf9867d93a690ccd36dbe9
Rathod Raj <rathodraj1504@gmail.com>
2026-04-10 23:58:53 +0530

    Refactor API URL for image upload

--- client/src/Services.js
+++ client/src/Services.js
@@ -39,7 +39,9 @@ function Service() {
     const formData = new FormData();
     formData.append("file", selectedFile);
 
-    fetch("https://flowerdiseasesystem.onrender.com/image/upload", {
+    const API_URL = "https://flowerdiseasesystem.onrender.com";
+
+    fetch(`${API_URL}/image/upload`, {
       method: "POST",
       body: formData,
     })
